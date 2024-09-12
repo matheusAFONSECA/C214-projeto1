ecfec503a3ab519525cfcfa824f9a642402a4382
Álvaro Lúcio Almeida Ribeiro <alvaroluciorib@gmail.com>
2024-09-11 23:09:33 -0300

    feat(project): creating new tests
    Created new tests, where one
    the room number is written
    and not in full, another
    empty test and the last
    with an invalid building.

--- src/tests/test_professor_data.py
+++ src/tests/test_professor_data.py
@@ -23,6 +23,46 @@ class TestProfessorData(unittest.TestCase):
         }
         with self.assertRaises(KeyError):
             process_professor_data(incomplete_data)
+            
+    def test_process_professor_data_invalid_room(self):
+        """
+        Failure case: Test when 'sala' is invalid (string instead of int).
+        """
+        invalid_data = {
+            "prof1": {
+                "nomeDoProfessor": "Marcelo Cisneyros",
+                "horarioDeAtendimento": "19:30",
+                "periodo": "Noturno",
+                "sala": "three",
+                "predio": ["1", "2", "3", "4", "6"],
+            }
+        }
+        with self.assertRaises(ValueError):
+            process_professor_data(invalid_data)
+
+    def test_process_professor_data_empty_data(self):
+        """
+        Failure case: Test when data is empty.
+        """
+        empty_data = {}
+        with self.assertRaises(ValueError):
+            process_professor_data(empty_data)
+
+    def test_process_professor_data_invalid_predio_type(self):
+        """
+        Failure case: Test when 'predio' is not a list.
+        """
+        invalid_data = {
+            "prof1": {
+                "nomeDoProfessor": "Marcelo Cisneyros",
+                "horarioDeAtendimento": "19:30",
+                "periodo": "Noturno",
+                "sala": "3",
+                "predio": "1234",  
+            }
+        }
+        with self.assertRaises(TypeError):
+            process_professor_data(invalid_data)
 
 
 if __name__ == "__main__":
